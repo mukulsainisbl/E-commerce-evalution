a2135a243aea7899413f85b8d2da4bb1c72e410f
Mukul Saini <138042294+mukulsainisbl@users.noreply.github.com>
2024-06-02 17:57:47 +0530

    Private Route

--- src/components/AllRoutes.jsx
+++ src/components/AllRoutes.jsx
@@ -2,21 +2,35 @@ import { Routes,Route } from "react-router-dom"
 import Home from "../pages/Home"
 import Login from "../pages/Login"
 import Products from "../pages/ProductsDetailsPage"
-
+import PrivateRoute from "./PrivateRoute"
 const AllRoutes = () => {
 
 
   return (
     <Routes>
+      
         <Route path="/" element={
-       
- <Home/>
+    <PrivateRoute>
+      <Home/>
+    </PrivateRoute>   
+ 
        
        
         
         }/>
-        <Route path="/products/:id" element={<Products/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/products/:id" element={
+     
+            <Products/>
+       
+      
+        
+        }/>
+        <Route path="/login" element={
+       
+             <Login/>
+       
+     
+        }/>
     </Routes>
    
   )
